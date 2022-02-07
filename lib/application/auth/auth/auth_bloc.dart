42ab9d1f4385e9e31c2b5259089f308013991a3b
Roman <51263277+Spriggan1992@users.noreply.github.com>
2022-02-07 17:38:44 +0300

    [ASTRA-1592] feature: Implement service for getting curator information. (#30)

--- lib/application/auth/auth/auth_bloc.dart
+++ lib/application/auth/auth/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:astra_app/domain/auth/repositories/i_auth_api_service.dart';
+import 'package:astra_app/domain/core/services/i_curator_info_service.dart';
 import 'package:astra_app/domain/core/services/i_user_unfo_service.dart';
 import 'package:astra_app/domain/profile/repositories/i_profile_repository.dart';
 import 'package:bloc/bloc.dart';
@@ -12,37 +13,49 @@ part 'auth_bloc.freezed.dart';
 @injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final IUserInfoService _userService;
+  final ICuratorInfoService _curatorService;
   final IProfileRepository _profileRepository;
   final IAuthApiService _apiService;
-  AuthBloc(this._apiService, this._profileRepository, this._userService)
-      : super(const AuthState.initial()) {
-    on<AuthEvent>((event, emit) async {
-      await event.map(
-        authCheckRequested: (e) async {
-          await Future.delayed(const Duration(milliseconds: 500), () => true);
-          // final result = await _profileRepository.getProfile();
-          final result = await _apiService.isSignIn();
-          if (result) {
-            // emit(const AuthState.loading());
-            final profileResponse = await _profileRepository.getProfile();
-            emit(
-              profileResponse.fold(
-                (failure) => const AuthState.unauthenticated(),
-                (proifile) {
-                  _userService.setUserProfile(proifile);
-                  return const AuthState.authenticated();
-                },
-              ),
-            );
-          } else {
+  AuthBloc(
+    this._apiService,
+    this._profileRepository,
+    this._userService,
+    this._curatorService,
+  ) : super(
+          const AuthState.initial(),
+        ) {
+    on<AuthEvent>(
+      (event, emit) async {
+        await event.map(
+          authCheckRequested: (e) async {
+            await Future.delayed(const Duration(milliseconds: 500), () => true);
+            final result = await _apiService.isSignIn();
+            if (result) {
+              final profileResponse = await _profileRepository.getProfile();
+              final curatorResponse = await _profileRepository.getCuratorInfo();
+              emit(
+                profileResponse.fold(
+                  (failure) => const AuthState.unauthenticated(),
+                  (proifile) {
+                    _userService.setUserProfile(proifile);
+                    curatorResponse.fold(
+                      (failure) => const AuthState.unauthenticated(),
+                      (curator) => _curatorService.setCuratorProfile(curator),
+                    );
+                    return const AuthState.authenticated();
+                  },
+                ),
+              );
+            } else {
+              emit(const AuthState.unauthenticated());
+            }
+          },
+          signedOut: (e) async {
+            await _apiService.signOut();
             emit(const AuthState.unauthenticated());
-          }
-        },
-        signedOut: (e) async {
-          await _apiService.signOut();
-          emit(const AuthState.unauthenticated());
-        },
-      );
-    });
+          },
+        );
+      },
+    );
   }
 }
