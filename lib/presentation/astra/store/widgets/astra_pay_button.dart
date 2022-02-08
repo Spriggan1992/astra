f8712495df391e54304411e62dd7b9a08cc8d7b9
Pavel Shalimov <pabushabi1@gmail.com>
2022-02-08 13:58:19 +0900

    [Astra-1500] Added ApplePay & GooglePay integration
    bumped targetSdkVersion
    updated recommended settings for ios
    fixed svg render

---
+++ lib/presentation/astra/store/widgets/astra_pay_button.dart
@@ -0,0 +1,98 @@
+import 'package:astra_app/application/settings/store/store_actor/store_actor_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:pay/pay.dart';
+import 'dart:developer';
+
+class AstraPayButton extends StatelessWidget {
+  final BuildContext context;
+  final StoreActorState state;
+  final ValueSetter<Map<String, dynamic>> onApplePayResult;
+  final ValueSetter<Map<String, dynamic>> onGooglePayResult;
+  final VoidCallback? onPressed;
+
+  /// Implements ApplePay & GooglePay buttons and renders proper button
+  /// depending of platform and StoreActorState [state]
+  /// TODO: Add valid merchant info into configs
+  const AstraPayButton({
+    Key? key,
+    required this.context,
+    required this.state,
+    required this.onApplePayResult,
+    required this.onGooglePayResult,
+    this.onPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        aPayButton(context, state),
+        gPayButton(context, state),
+      ],
+    );
+  }
+
+  Widget aPayButton(BuildContext context, StoreActorState state) {
+    return Column(
+      children: List.generate(
+        state.likes.length,
+        (index) => (state.like.id == state.likes[index].id)
+            ? ApplePayButton(
+                paymentConfigurationAsset:
+                    'payment_configs/applepay_config.json',
+                onPaymentResult: onApplePayResult,
+                paymentItems: [
+                  PaymentItem(
+                    amount: state.likes[index].price.toString(),
+                    label: "Покупка ${state.likes[index].amount} лайков",
+                    status: PaymentItemStatus.final_price,
+                    type: PaymentItemType.total,
+                  ),
+                ],
+                width: MediaQuery.of(context).size.width -
+                    (MediaQuery.of(context).size.width / 5),
+                height: 55,
+                type: ApplePayButtonType.buy,
+                style: ApplePayButtonStyle.automatic,
+                onPressed: onPressed,
+                onError: (err) => log("$err", name: "Apple pay error"),
+                loadingIndicator: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            : Container(),
+      ),
+    );
+  }
+
+  Widget gPayButton(BuildContext context, StoreActorState state) {
+    return Column(
+      children: List.generate(
+        state.likes.length,
+        (index) => (state.like.id == state.likes[index].id)
+            ? GooglePayButton(
+                paymentConfigurationAsset: 'payment_configs/gpay_config.json',
+                onPaymentResult: onGooglePayResult,
+                paymentItems: [
+                  PaymentItem(
+                    amount: state.likes[index].price.toString(),
+                    label: "Покупка ${state.likes[index].amount} лайков",
+                    status: PaymentItemStatus.final_price,
+                    type: PaymentItemType.total,
+                  ),
+                ],
+                width: MediaQuery.of(context).size.width -
+                    (MediaQuery.of(context).size.width / 5),
+                height: 55,
+                type: GooglePayButtonType.buy,
+                onPressed: onPressed,
+                onError: (err) => log("$err", name: "Google pay error"),
+                loadingIndicator: const Center(
+                  child: CircularProgressIndicator(),
+                ),
+              )
+            : Container(),
+      ),
+    );
+  }
+}
